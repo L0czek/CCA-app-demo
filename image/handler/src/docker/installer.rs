3f18b9bcdc271d4ab5eb606de647166d78865bb7
Michał Szaknis <m.szaknis@samsung.com>
2024-04-03 20:35:47 +0200

    Start implementing docker image launcher
    Signed-off-by: Michał Szaknis <m.szaknis@samsung.com>

--- image/handler/src/docker/installer.rs
+++ image/handler/src/docker/installer.rs
@@ -1,11 +1,25 @@
 use std::path::{Path, PathBuf};
 
 use thiserror::Error;
+use tokio::fs::create_dir;
+use tokio_tar::Archive;
+
+use crate::{ImageError, InstallerTrait};
 
 
 #[derive(Error, Debug)]
 pub enum InstallerError {
+    #[error("Directory creation error")]
+    DirCreationError(#[source] std::io::Error),
 
+    #[error("Archive unpack error")]
+    ArchiveError(#[source] std::io::Error)
+}
+
+impl From<InstallerError> for ImageError {
+    fn from(value: InstallerError) -> Self {
+        ImageError::DockerError(value.into())
+    }
 }
 
 pub type Result<V> = std::result::Result<V, InstallerError>;
@@ -14,12 +28,21 @@ pub struct Installer {
     dst: PathBuf
 }
 
-impl Installer {
-    pub fn target(path: &Path) -> Self {
-        Self {
-            dst: PathBuf::from(path)
-        }
+impl InstallerTrait for Installer {
+    fn target(path: &Path) -> Self {
+        Self { dst: PathBuf::from(path) }
     }
 
+    fn install(&self, image: impl tokio::io::AsyncRead + Unpin) -> impl std::future::Future<Output =  crate::Result<()>> {
+        async move {
+            let mut archive = Archive::new(image);
+            let dir = self.dst.join("img");
+            create_dir(&dir).await.map_err(InstallerError::DirCreationError)?;
+            archive.unpack(&dir).await.map_err(InstallerError::ArchiveError)?;
 
+
+
+            Ok(())
+        }
+    }
 }
--- image/handler/src/lib.rs
+++ image/handler/src/lib.rs
@@ -1,5 +1,28 @@
 mod hasher;
 mod docker;
 
+use std::future::Future;
+use std::path::Path;
+
 pub use hasher::Hasher;
 pub use hasher::HashType;
+use thiserror::Error;
+use tokio::io::AsyncRead;
+
+#[derive(Error, Debug)]
+pub enum ImageError {
+    #[error("Docker image error")]
+    DockerError(#[from] docker::DockerError)
+}
+
+pub type Result<T> = std::result::Result<T, ImageError>;
+
+pub trait InstallerTrait {
+    fn target(path: &Path) -> Self;
+    fn install(&self, image: impl AsyncRead + Unpin) -> impl Future<Output =  Result<()>>;
+}
+
+pub trait Launcher {
+    fn rootfs(path: Path) -> Self;
+}
+
