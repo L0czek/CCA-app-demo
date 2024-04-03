c29da31cb45583f2a763f0000d0369441a35cdb0
Michał Szaknis <m.szaknis@samsung.com>
2024-04-03 20:35:47 +0200

    Start implementing docker image launcher
    Signed-off-by: Michał Szaknis <m.szaknis@samsung.com>

---
+++ image/handler/src/docker/manifests.rs
@@ -0,0 +1,155 @@
+use std::fmt::Display;
+
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use serde::de::Error;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum MediaType {
+    #[serde(rename = "application/vnd.docker.distribution.manifest.v1+json")]
+    OldManifest,
+
+    #[serde(rename = "application/vnd.docker.distribution.manifest.v2+json")]
+    NewManifest,
+
+    #[serde(rename = "application/vnd.docker.distribution.manifest.list.v2+json")]
+    ManifestList,
+
+    #[serde(rename = "application/vnd.docker.container.image.v1+json")]
+    ContainerConfig,
+
+    #[serde(rename = "application/vnd.docker.image.rootfs.diff.tar.gzip")]
+    Layer,
+
+    #[serde(rename = "application/vnd.docker.image.rootfs.foreign.diff.tar.gzip")]
+    ForeignLayer,
+
+    #[serde(rename = "application/vnd.docker.plugin.v1+json")]
+    PluginConfig
+}
+
+#[derive(Debug)]
+pub enum HashType {
+    Sha256,
+    Sha512
+}
+
+impl Display for HashType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            HashType::Sha256 => write!(f, "sha256"),
+            HashType::Sha512 => write!(f, "sha512")
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Digest {
+    pub ty: HashType,
+    pub val: Vec<u8>
+}
+
+impl Serialize for Digest {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer {
+        serializer.serialize_str(format!("{}:{}", &self.ty, hex::encode(&self.val)).as_str())
+    }
+}
+
+
+impl<'de> Deserialize<'de> for Digest {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de> {
+        let v: &str = Deserialize::deserialize(deserializer)?;
+        let (ty, val) = v.split_once(':')
+            .ok_or(D::Error::invalid_value(serde::de::Unexpected::Str(v), &"Expected hash_type:hash_value_hex"))?;
+
+        let ty = match ty {
+            x if x == "sha256" => HashType::Sha256,
+            x if x == "sha512" => HashType::Sha512,
+            _ => {
+                return Err(D::Error::invalid_value(serde::de::Unexpected::Str(ty), &"sha256 or sha512"));
+            }
+        };
+
+        let val = hex::decode(val)
+            .map_err(|_| D::Error::invalid_value(serde::de::Unexpected::Str(val), &"Expected hex string"))?;
+
+        Ok(Self {
+            ty,
+            val
+        })
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Object {
+    #[serde(rename = "mediaType")]
+    pub media_type: MediaType,
+
+    pub digest: Digest,
+    pub size: usize
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Manifest {
+    #[serde(rename = "schemaVersion")]
+    pub schema_version: usize,
+
+    #[serde(rename = "mediaType")]
+    pub media_type: MediaType,
+
+    pub config: Object,
+    pub layers: Vec<Object>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LaunchConfig {
+    #[serde(rename = "Env")]
+    pub env: Vec<String>,
+
+    #[serde(rename = "Cmd")]
+    pub cmd: Option<Vec<String>>,
+
+    #[serde(rename = "WorkingDir")]
+    pub pwd: String,
+
+    #[serde(rename = "ArgsEscaped")]
+    pub args_escaped: bool,
+
+    #[serde(rename = "OnBuild")]
+    pub on_build: Option<String>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct HistoryItem {
+    pub created: DateTime<Utc>,
+    pub created_by: String,
+    pub comment: Vec<String>,
+    pub empty_layer: bool
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RootFs {
+    #[serde(rename = "type")]
+    pub ty: String,
+
+    pub diff_ids: Vec<Digest>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ContainerConfig {
+    #[serde(rename = "architecture")]
+    pub arch: String,
+
+    pub created: DateTime<Utc>,
+    pub os: String,
+
+    pub config: LaunchConfig,
+    pub history: Vec<HistoryItem>,
+    pub rootfs: RootFs
+
+}
--- image/handler/src/docker/mod.rs
+++ image/handler/src/docker/mod.rs
@@ -1,1 +1,10 @@
+use thiserror::Error;
+
 mod installer;
+mod manifests;
+
+#[derive(Error, Debug)]
+pub enum DockerError {
+    #[error("Docker installer error")]
+    InstallerError(#[from] installer::InstallerError)
+}
